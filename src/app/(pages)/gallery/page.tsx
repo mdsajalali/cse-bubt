e6eb6bcba209309aa98313a6623d0bdf21cb12e0
mdsajalali <mdsajalali2020@gmail.com>
2024-12-11 23:52:07 +0600

    add: gallery card

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -1,4 +1,7 @@
+import GalleryContent from "@/components/core/gallery/GalleryContent";
+import Container from "@/components/shared/Container";
 import Cover from "@/components/shared/Cover";
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
@@ -10,6 +13,38 @@ export default function page() {
         mainTitle="home"
         titleLink="/"
       />
+      <Container>
+        <div className="flex items-center flex-wrap gap-5 py-10">
+          <div className="group rounded border p-2">
+            <div className="overflow-hidden rounded">
+              <Image
+                src="/gallery/four.jpeg"
+                alt="image"
+                width={500}
+                height={300}
+                className="transform rounded transition duration-300 group-hover:scale-110"
+              />
+            </div>
+            <h1 className="pt-1 text-center text-[20px] font-medium">
+              BUBT 5th Convocation at BCCI
+            </h1>
+          </div>
+          <div className="group rounded border p-2">
+            <div className="overflow-hidden rounded">
+              <Image
+                src="/gallery/six.jpeg"
+                alt="image"
+                width={500}
+                height={300}
+                className="transform rounded transition duration-300 group-hover:scale-110"
+              />
+            </div>
+            <h1 className="pt-1 text-center text-[20px] font-medium">
+              Our Beauty Campus
+            </h1>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
---
+++ src/components/core/gallery/GalleryContent.tsx
@@ -0,0 +1,71 @@
+"use client";
+import LightGallery from "lightgallery/react";
+import lgZoom from "lightgallery/plugins/zoom";
+
+// import styles
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+export default function GalleryContent() {
+  return (
+    <LightGallery plugins={[lgZoom]} speed={500} elementClassNames={"gallery"}>
+      <a
+        className={"gallery__item"}
+        data-lg-size="400-600-375, 600-900-480, 1600-2400"
+        data-src="https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80"
+        data-responsive="https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80 375, https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=80 480"
+        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@therawhunter' >Massimiliano Morosinotto </a></h4><p> Location - <a href='https://unsplash.com/s/photos/tre-cime-di-lavaredo%2C-italia'>Tre Cime di Lavaredo, Italia</a>This is the Way</p>"
+      >
+        <img
+          className="img-fluid"
+          src="https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80"
+        />
+      </a>
+      <a
+        className={"gallery__item"}
+        data-lg-size="400-600-375, 600-900-480, 1600-2400"
+        data-src="https://images.unsplash.com/photo-1605973029521-8154da591bd7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80"
+        data-responsive="https://images.unsplash.com/photo-1605973029521-8154da591bd7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80 375, https://images.unsplash.com/photo-1605973029521-8154da591bd7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=80 480"
+        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@thesaboo' >Sascha Bosshard </a></h4><p> Location - <a href='https://unsplash.com/s/photos/pizol%2C-mels%2C-schweiz'>Pizol, Mels, Schweiz</a></p>"
+      >
+        <img
+          className="img-fluid"
+          src="https://images.unsplash.com/photo-1605973029521-8154da591bd7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80"
+        />
+      </a>
+      <a
+        className={"gallery__item"}
+        data-lg-size="400-600-375, 600-899-480, 1600-2398"
+        data-src="https://images.unsplash.com/photo-1526281216101-e55f00f0db7a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1600&q=80"
+        data-responsive="https://images.unsplash.com/photo-1526281216101-e55f00f0db7a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80 375, https://images.unsplash.com/photo-1526281216101-e55f00f0db7a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=80 480"
+        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@yusufevli' >Yusuf Evli </a></h4><p> Foggy Road</p>"
+      >
+        <img
+          className="img-fluid"
+          src="https://images.unsplash.com/photo-1526281216101-e55f00f0db7a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80"
+        />
+      </a>
+      <a
+        data-lg-size="400-267-375, 600-400-480, 1600-1067"
+        data-src="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1600&amp;q=80"
+        data-responsive="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=400&amp;q=100 375, https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=600&amp;q=100 480"
+        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@tobbes_rd' >Tobias Rademacher </a></h4><p> Location - <a href='https://unsplash.com/s/photos/puezgruppe%2C-wolkenstein-in-gr%C3%B6den%2C-s%C3%BCdtirol%2C-italien'>Puezgruppe, Wolkenstein in Gröden, Südtirol, Italien</a>layers of blue.</p>"
+        data-slide-name="fading-light"
+        data-tweet-text="lightGallery slide  1"
+        data-twitter-share-url="share/twitter-share-url"
+        data-reddit-title="Fading Light"
+        data-disqus-identifier="photo-1609342122563-a43ac8917a3a"
+        data-disqus-url="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1600&amp;q=80"
+        data-fb-html='<div class="fb-comments" data-href="http://sachinchoolur.github.io/lightGallery/demos/comment-box.html#lg=1&amp;slide=0" data-width="400" data-numposts="5"></div>'
+        className="jg-entry jg-entry-visible"
+        data-lg-id="d732e049-5fbf-452b-a704-acc8ae9bb7a3"
+      >
+        <img
+          className="img-responsive"
+          src="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=240&amp;q=80"
+        />
+      </a>
+    </LightGallery>
+  );
+}
