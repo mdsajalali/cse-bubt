9521ae260cfc845fb18933048c6eb4d52d9259c8
mdsajalali <mdsajalali2020@gmail.com>
2024-12-11 23:26:26 +0600

    add: contact form

--- src/app/(common)/contact/page.tsx
+++ src/app/(common)/contact/page.tsx
@@ -1,9 +1,16 @@
+import ContactForm from "@/components/core/contact/ContactForm";
 import Cover from "@/components/shared/Cover";
 
 export default function page() {
   return (
     <div>
-      <Cover heading="Contacts" title="Contact" mainTitle="home" titleLink="/" />
+      <Cover
+        heading="Contacts"
+        title="Contact"
+        mainTitle="home"
+        titleLink="/"
+      />
+      <ContactForm />
     </div>
   );
 }
