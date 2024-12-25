982ed194cf87c03db608e0489e42d0292bd1b737
mdsajalali <mdsajalali2020@gmail.com>
2024-12-25 22:41:07 +0600

    add: top nav tooltip

--- src/components/core/nav/TopNavbar.tsx
+++ src/components/core/nav/TopNavbar.tsx
@@ -4,14 +4,14 @@ import NavSlider from "./NavSidebar";
 import Link from "next/link";
 
 const iconData = [
-  { src: "/icons/nx.png", alt: "nx" },
-  { src: "/icons/ph.png", alt: "ph" },
-  { src: "/icons/mail.png", alt: "mail" },
-  { src: "/icons/mc.png", alt: "mc" },
-  { src: "/icons/login.png", alt: "login" },
-  { src: "/icons/lib.png", alt: "lib" },
-  { src: "/icons/e.png", alt: "e" },
-  { src: "/icons/faq.png", alt: "faq" },
+  { src: "/icons/nx.png", alt: "nx", link: "https://annex.bubt.edu.bd/" },
+  { src: "/icons/ph.png", alt: "ph", link: "0123456789" },
+  { src: "/icons/mail.png", alt: "mail", link: "info@bubt.edu.bd" },
+  { src: "/icons/mc.png", alt: "mc", link: "https://cse.bubt.edu.bd/" },
+  { src: "/icons/login.png", alt: "login", link: "https://cse.bubt.edu.bd/" },
+  { src: "/icons/lib.png", alt: "lib", link: "https://cse.bubt.edu.bd" },
+  { src: "/icons/e.png", alt: "e", link: "all-notices" },
+  { src: "/icons/faq.png", alt: "faq", link: "https://cse.bubt.edu.bd/" },
 ];
 
 export default function TopNavbar() {
@@ -33,15 +33,47 @@ export default function TopNavbar() {
           <div className="w-full md:w-auto">
             <div className="flex flex-col items-center gap-2 md:flex-row md:items-center">
               <div className="flex flex-wrap justify-center gap-4">
-                {iconData.map(({ src, alt }) => (
-                  <div className="relative mt-3 h-5 w-5 xl:mt-0">
-                    <Image
-                      key={alt}
-                      src={src}
-                      alt={alt}
-                      fill
-                      className="object-cover"
-                    />
+                {iconData.map((icon, index) => (
+                  <div
+                    key={icon.alt}
+                    className="group relative mt-3 h-5 w-5 xl:mt-0"
+                  >
+                    {/* Icon Link */}
+                    <a
+                      href={icon.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="block"
+                    >
+                      <Image
+                        src={icon.src}
+                        alt={icon.alt}
+                        fill
+                        className="object-cover"
+                      />
+                    </a>
+
+                    {/* Tooltip */}
+                    <div
+                      className={`absolute ${
+                        index % 2 === 0
+                          ? "bottom-full translate-y-2"
+                          : "top-full translate-y-2"
+                      } left-1/2 -translate-x-1/2 transform rounded-lg bg-gray-800 px-2 py-1 text-xs font-medium text-white opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100`}
+                    >
+                      {icon.alt}
+
+                      {/* Tooltip Arrow */}
+                      <div
+                        className={`absolute ${
+                          index % 2 === 0 ? "bottom-0" : "top-0"
+                        } left-1/2 h-0 w-0 -translate-x-1/2 transform border-b-8 border-t-8 border-x-transparent ${
+                          index % 2 === 0
+                            ? "border-b-gray-800"
+                            : "border-t-gray-800"
+                        } transition-all delay-150 duration-200`}
+                      ></div>
+                    </div>
                   </div>
                 ))}
                 <div className="xl:hidden">
