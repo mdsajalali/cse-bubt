65dc2f2f1a46eec88919a8d376163350949e5ffe
mdsajalali <mdsajalali2020@gmail.com>
2024-12-26 00:20:35 +0600

    add: tooltip arrow

--- src/components/core/nav/TopNavbar.tsx
+++ src/components/core/nav/TopNavbar.tsx
@@ -10,7 +10,7 @@ const iconData = [
   { src: "/icons/mc.png", alt: "mc", link: "https://cse.bubt.edu.bd/" },
   { src: "/icons/login.png", alt: "login", link: "https://cse.bubt.edu.bd/" },
   { src: "/icons/lib.png", alt: "lib", link: "https://cse.bubt.edu.bd" },
-  { src: "/icons/e.png", alt: "e", link: "all-notices" },
+  { src: "/icons/e.png", alt: "cal", link: "all-notices" },
   { src: "/icons/faq.png", alt: "faq", link: "https://cse.bubt.edu.bd/" },
 ];
 
@@ -31,13 +31,18 @@ export default function TopNavbar() {
             </Link>
           </div>
           <div className="w-full md:w-auto">
-            <div className="flex flex-col items-center gap-2 md:flex-row md:items-center">
-              <div className="flex flex-wrap justify-center gap-4">
-                {iconData.map((icon, index) => (
-                  <div
-                    key={icon.alt}
-                    className="group relative mt-3 h-5 w-5 xl:mt-0"
-                  >
+            <div className="text-center">
+              <h2 className="text-[18px] font-semibold text-gray-800">
+                Department of
+              </h2>
+              <h1 className="pb-2 text-2xl font-semibold text-primary">
+                Computer Science and Engineering
+              </h1>
+            </div>
+            <div className="mb-2 flex flex-col items-center gap-2 md:flex-row md:items-center">
+              <div className="flex flex-wrap items-center justify-center gap-[10px] md:items-start md:gap-4">
+                {iconData.map((icon) => (
+                  <div key={icon.alt} className="group relative">
                     {/* Icon Link */}
                     <a
                       href={icon.link}
@@ -48,31 +53,17 @@ export default function TopNavbar() {
                       <Image
                         src={icon.src}
                         alt={icon.alt}
-                        fill
+                        width={24}
+                        height={24}
                         className="object-cover"
                       />
                     </a>
 
                     {/* Tooltip */}
-                    <div
-                      className={`absolute ${
-                        index % 2 === 0
-                          ? "bottom-full translate-y-2"
-                          : "top-full translate-y-2"
-                      } left-1/2 -translate-x-1/2 transform rounded-lg bg-gray-800 px-2 py-1 text-xs font-medium text-white opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100`}
-                    >
+                    <div className="absolute bottom-full left-1/2 mb-2 hidden -translate-x-1/2 transform rounded bg-gray-800 px-2 py-1 text-sm font-medium text-white shadow-md transition-opacity duration-300 ease-in-out group-hover:block">
                       {icon.alt}
-
-                      {/* Tooltip Arrow */}
-                      <div
-                        className={`absolute ${
-                          index % 2 === 0 ? "bottom-0" : "top-0"
-                        } left-1/2 h-0 w-0 -translate-x-1/2 transform border-b-8 border-t-8 border-x-transparent ${
-                          index % 2 === 0
-                            ? "border-b-gray-800"
-                            : "border-t-gray-800"
-                        } transition-all delay-150 duration-200`}
-                      ></div>
+                      {/* Arrow */}
+                      <div className="absolute bottom-0 left-1/2 -mb-1 -translate-x-1/2 transform border-x-8 border-b-0 border-t-8 border-x-transparent border-t-gray-800"></div>
                     </div>
                   </div>
                 ))}
@@ -81,17 +72,9 @@ export default function TopNavbar() {
                 </div>
               </div>
               <input
-                className="mt-2 w-full rounded-lg border border-gray-300 bg-gray-100 px-4 py-2 text-sm text-gray-700 placeholder-gray-400 focus:border-primary focus:bg-white focus:outline-none focus:ring-1 focus:ring-primary md:mt-0 md:w-auto"
+                className="mt-2 hidden w-full rounded-lg border border-gray-300 bg-gray-100 px-4 py-2 text-sm text-gray-700 placeholder-gray-400 focus:border-primary focus:bg-white focus:outline-none focus:ring-1 focus:ring-primary md:mt-0 md:w-auto xl:block"
                 placeholder="Search..."
               />
-            </div>
-            <div className="mt-4 text-center md:mt-2">
-              <h2 className="text-[18px] font-semibold text-gray-800">
-                Department of
-              </h2>
-              <h1 className="pb-2 text-2xl font-semibold text-primary">
-                Computer Science and Engineering
-              </h1>
             </div>
           </div>
         </div>
